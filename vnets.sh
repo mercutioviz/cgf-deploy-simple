0cc835baad1a791d258f79fc5d496000b417d55a
Michael S Collins <mcollins@barracuda.com>
2021-12-09 19:21:15 +0000

    Tweak

--- vnets.sh
+++ vnets.sh
@@ -15,8 +15,8 @@ vnet_count=$((`echo ${vnet_json} | jq '. | length'`-1))
 myvnet='VNET-E2US'
 for I in `seq 0 "${vnet_count}"`; do
   this_vnet=`echo "$vnet_json" | jq -r --arg 'I' $I '.[$I | tonumber] | .name'`
-  if [[ "${this_vnet}" == "${myvnet}" ]]; then
+  ##if [[ "${this_vnet}" == "${myvnet}" ]]; then
     echo -n "${I}: "
     echo "${this_vnet}"
-  fi
+  ##fi
 done
